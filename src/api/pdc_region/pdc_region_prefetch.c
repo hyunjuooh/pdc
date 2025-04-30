c5e4ade27175426bb58834de900b2c52edd5a7fe
Hyunju Oh <hjoh16@login34.chn.perlmutter.nersc.gov>
2025-04-30 09:56:44 -0700

    Send prefetch hint by objid

--- src/api/pdc_region/pdc_region_prefetch.c
+++ src/api/pdc_region/pdc_region_prefetch.c
@@ -16,103 +16,40 @@
 
 pdcid_t *obj_prefetch_list;
 int      obj_prefetch_list_len;
-int *    obj_idx_array;
 
 perr_t
-PDCregion_receive_prefetch_hint(int *obj_array, int obj_array_len)
+PDCregion_receive_prefetch_hint(const pdcid_t* arr, int obj_array_len)
 {
     perr_t ret_value = SUCCEED;
-    int    i, sample_per_rank, item_idx = 0;
-    char   obj_filename[2000];
+    
+    int    i, item_idx = 0;
+    int    current_rank = pdc_client_mpi_rank_g;
+    pdcid_t obj_item;
 
     FUNC_ENTER(NULL);
 
-    if (pdc_client_mpi_rank_g == 0) {
-        printf("PDCregion_receive_prefetch_hint: ");
-        fflush(stdout);
-        // for (i = 0; i < obj_array_len; i++) {
-        //     printf("%s, \n", filename[obj_array[i]]);
-        //     snprintf(obj_filename, sizeof(obj_filename), "%s%s", filename[obj_array[i]], "-records");
-        //     printf("%s, \n", obj_filename);
-        //     fflush(stdout);
-        // }
-    }
+    // for (i = 0; i < obj_array_len; i++) {
+    //     printf("Received from C: %s\n", arr[i]);
+    // }
 
     obj_prefetch_list_len = obj_array_len;
     obj_prefetch_list     = (pdcid_t *)PDC_malloc(obj_prefetch_list_len * sizeof(pdcid_t));
+    memcpy(obj_prefetch_list, arr, obj_prefetch_list_len);
 
-    sample_per_rank = obj_prefetch_list_len / pdc_client_mpi_size_g;
+    if (pdc_client_mpi_rank_g == 0)
+        printf("[RANK %d] PDCregion_receive_prefetch_hint - object list item length %d \n", pdc_client_mpi_rank_g, obj_prefetch_list_len);
 
-    // Get the list of object id for each rank that should be prefetched
-    for (i = pdc_client_mpi_rank_g * sample_per_rank; i < (pdc_client_mpi_rank_g + 1) * sample_per_rank;
-         i++) {
-        snprintf(obj_filename, sizeof(obj_filename), "%s%s", filename[obj_array[i]], "-records");
-        obj_prefetch_list[item_idx] = PDCobj_open(obj_filename, pdc_id);
-        PDCobj_close(obj_prefetch_list[item_idx]);
-
-        item_idx++;
-    }
-
-done:
-    fflush(stdout);
-    FUNC_LEAVE(ret_value);
-}
-
-perr_t
-PDCregion_receive_dataset(const char **dataset_array, int obj_array_len)
-{
-    perr_t ret_value = SUCCEED;
-    int    i;
-
-    FUNC_ENTER(NULL);
-
-    if (pdc_client_mpi_rank_g == 0) {
-        printf("PDCregion_receive_prefetch_hint: ");
-        fflush(stdout);
-        for (i = 0; i < obj_array_len; i++) {
-            printf("%s, \n", dataset_array[i]);
-            fflush(stdout);
-        }
-    }
+    // // Get the list of object id for each rank that should be prefetched
+    // for (i = 0; i < obj_prefetch_list_len; i++) {
+    //     obj_item = PDCobj_open()
+    //     obj_prefetch_list[item_idx] = obj_array[i];
+    //     item_idx++;
+    // }
 
 done:
     fflush(stdout);
     FUNC_LEAVE(ret_value);
 }
-
-// perr_t
-// PDCregion_receive_prefetch_hint(const char **obj_array, int obj_array_len)
-// {
-//     perr_t ret_value = SUCCEED;
-//     int    i, sample_per_rank, item_idx = 0;
-
-//     FUNC_ENTER(NULL);
-
-//     if (pdc_client_mpi_rank_g == 0) {
-//         for (i = 0; i < obj_array_len; i++) {
-//             printf("PDCregion_receive_prefetch_hint %d: %s\n", i, obj_array[i]);
-//             fflush(stdout);
-//         }
-//     }
-
-//     // obj_prefetch_list_len = obj_array_len;
-//     // obj_prefetch_list     = (pdcid_t *)PDC_malloc(obj_prefetch_list_len * sizeof(pdcid_t));
-
-//     // sample_per_rank = obj_prefetch_list_len / pdc_client_mpi_size_g;
-
-//     // // Get the list of object id for each rank that should be prefetched
-//     // for (i = pdc_client_mpi_rank_g * sample_per_rank; i < (pdc_client_mpi_rank_g + 1) * sample_per_rank;
-//     //      i++) {
-//     //     obj_prefetch_list[item_idx] = PDCobj_open(obj_array[i], pdc_id);
-//     //     PDCobj_close(obj_prefetch_list[item_idx]);
-
-//     //     item_idx++;
-//     // }
-
-// done:
-//     fflush(stdout);
-//     FUNC_LEAVE(ret_value);
-// }
 
 perr_t
 PDCregion_prefetch_by_objid()
