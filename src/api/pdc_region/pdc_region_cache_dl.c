fc976e201bb147ea5f9e5427d7b757002bd38fa8
Hyunju Oh <hjoh16@login37.chn.perlmutter.nersc.gov>
2026-04-07 11:10:54 -0700

    approach 2 - modified cache size for dlio

--- src/api/pdc_region/pdc_region_cache_dl.c
+++ src/api/pdc_region/pdc_region_cache_dl.c
@@ -16,8 +16,8 @@
 #include "pdc_region_prefetch.h"
 #include "pdc_client_connect.h"
 
-// #define MAX_CACHE_SIZE 34359738368
-#define MAX_CACHE_SIZE 4294967296
+#define MAX_CACHE_SIZE 34359738368
+// #define MAX_CACHE_SIZE 4294967296
 // #define MAX_CACHE_SIZE 4294967296
 // #define MAX_CACHE_SIZE 268435456
 // #define MAX_CACHE_SIZE 536870912
